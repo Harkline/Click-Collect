d624ce74d53a237303231b452c40daef30fd2067
Harkline <35963363+Harkline@users.noreply.github.com>
2021-03-13 16:18:03 +0100

    Initialisation index.php
    -Ajout de la page d'accueil avec test de bootstrap

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Accueil</title>
+
+        <link rel="stylesheet" type="text/css" href="bootstrap-5.0.0/css/Bootstrap.css">    
+    </head>
+    <body>
+		<h1> TEST </h1>
+		<button class="btn btn-primary" >CLICK ME! </button>   
+    </body>
+</html>
