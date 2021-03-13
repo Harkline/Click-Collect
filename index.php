45516edbb9e33e7dced46ff343f1dd69a535466f
Harkline <35963363+Harkline@users.noreply.github.com>
2021-03-13 17:08:13 +0100

    Ajout header/footer - Ajout test script JS et Jquery
    - Ajout d'un Header/footer simple
    - Ajout d'un test javascript et d'un test avec Jquery

--- index.php
+++ index.php
@@ -1,12 +1,32 @@
 <!DOCTYPE html>
-<html>
-    <head>
-        <title>Accueil</title>
 
-        <link rel="stylesheet" type="text/css" href="bootstrap-5.0.0/css/Bootstrap.css">    
-    </head>
-    <body>
+<html lang="fr">
+
+<html>
+	<head>
+		<title>Accueil</title>
+		<meta charset="utf-8">
+		<link rel="stylesheet" type="text/css" href="bootstrap-5.0.0/css/Bootstrap.css">    
+	</head>
+	<body>
+	
+		<!-- Header --> 
+		<header>           
+			<h1>Click And Collect !</h1>
+		</header>
+		
 		<h1> TEST </h1>
-		<button class="btn btn-primary" >CLICK ME! </button>   
-    </body>
+		<button class="btn btn-primary" onclick="document.getElementById('demo').innerHTML = Date()">CLICK ME! </button>  
+		
+		<p id="demo"></p>
+		<p>This is a test paragraph</p>
+		
+		<!-- Footer -->
+		<footer id="Accueil_Footer"> 
+			<p>Copyright Click And Collect - Tous droits réservés</p>
+		</footer>
+
+		<script src="js/jquery-3.6.0.min.js"></script>
+		<script src="js/script.js"></script>
+	</body>
 </html>
