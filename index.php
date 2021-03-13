6af50f255e5adc3d6b33ee97379833a424253b61
Harkline <35963363+Harkline@users.noreply.github.com>
2021-03-13 18:28:04 +0100

    Ajout d'une barre de navigation bootstrap
    - Ajout d'une barre de navigation bootstrap avec comme éléments : Accueil, Produits, Se connecter

--- index.php
+++ index.php
@@ -6,15 +6,43 @@
 	<head>
 		<title>Accueil</title>
 		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<link rel="stylesheet" type="text/css" href="bootstrap-5.0.0/css/Bootstrap.css">    
 	</head>
 	<body>
-	
+
+		<link rel="shortcut icon" type="image/jpg" href="image/favicon.ico"/>
+
 		<!-- Header --> 
 		<header>           
 			<h1>Click And Collect !</h1>
 		</header>
 		
+		<!-- Barre de navigation Bootstrap--> 
+		<nav class="navbar navbar-expand-lg navbar-light bg-light">
+			
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			
+			<div class="collapse navbar-collapse" id="navbarSupportedContent">
+				<ul class="navbar-nav me-auto">
+					<li class="nav-item active">
+						<a class="nav-link" href="index.php">Accueil<span class="sr-only"></span></a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="produits.php">Produits</a>
+					</li>
+				</ul>
+				<ul class="navbar-nav ms-auto">
+					<li class="nav-item">
+						<a class="nav-link" href="connexion.php">Se connecter</a>
+					</li>
+				</ul>
+			</div>
+		</nav>
+			
+			
 		<h1> TEST </h1>
 		<button class="btn btn-primary" onclick="document.getElementById('demo').innerHTML = Date()">CLICK ME! </button>  
 		
