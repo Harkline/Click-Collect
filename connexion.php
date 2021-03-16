6e945e3bcf7cba6ce4cf7c51dee5c2f7a16b3b71
Yanis Levesque <yanislevesque@hotmail.fr>
2021-03-16 22:07:44 +0100

    Formulaire (html) ajouté à la page connexion.php

--- connexion.php
+++ connexion.php
@@ -1,1 +1,33 @@
 
+<!DOCTYPE html>
+
+<html lang="fr">
+
+<html>
+	<head>
+		<title>Connexion</title>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link rel="stylesheet" type="text/css" href="bootstrap-5.0.0/css/Bootstrap.css">    
+	</head>
+	<body>
+	
+		<form>
+			<fieldset>
+				<div class="form-group">
+					<label for="inputUsernameConnexion">Username</label>
+					<input type="username" class="form-control" id="inputUsernameConnexion" aria-describedby="emailHelp" placeholder="Enter username">
+				</div>
+				<div class="form-group">
+					<label for="inputPasswordConnexion">Password</label>
+					<input type="password" class="form-control" id="inputPasswordConnexion" placeholder="Enter Password">
+				</div>
+				<button type="submit" class="btn btn-primary">Submit</button>
+			</fieldset>
+		</form>
+		
+		
+		
+	
+	</body>
+</html>
