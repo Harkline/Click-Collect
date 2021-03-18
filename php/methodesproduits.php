fadfdb270c8ca5b4c702d65f9ddef2db0f3d4812
olimalt <olimalt85@live.fr>
2021-03-18 16:01:40 +0100

    Ajout des methodes dans le ficher php/methodesproduits.php

--- php/methodesproduits.php
+++ php/methodesproduits.php
@@ -1,5 +1,5 @@
 <?php 
-<?php
+
 
 try{
         $bdd =new PDO('mysql:host=localhost;dbname=demo;charset=utf8', 'pdo', 'pdo');
@@ -10,7 +10,7 @@ try{
       } catch(PDOException $e) {
           die('Erreur : ' . $e->getMessage());
       }
-	  ?>
+	  
 
 
 class User{
@@ -36,10 +36,40 @@ class User{
 		return $sth->fetchAll(); 
 	}	
 	
+	public Function getadminsbyid($identifiant){
+		$sth = $this->Bdd->prepare("
+									SELECT MDP FROM T_admins 
+									WHERE Identifiant=(:identifiant)");
+		$sth->execute(array(':identifiant' => $identifiant));
+			
+		return $sth->fetchAll();
+	}
+	
+	
+	public Function getclientsbyid($identifiant){
+		$sth = $this->Bdd->prepare("
+									SELECT Identifiant,MDP,Id_client FROM T_clients 
+									WHERE Identifiant=(:identifiant)");
+		$sth->execute(array(':identifiant' => $identifiant));
+			
+		return $sth->fetchAll();
+	}
+	
+	
+	public Function getcommandesproduitsbyid($id){
+		$sth = $this->Bdd->prepare("
+									SELECT id_produit,quantitee_commande FROM T_produits_commande 
+									WHERE id_commande=(:id)");
+		$sth->execute(array(':id' => $id));
+			
+		return $sth->fetchAll();
+	}
+	
+	
 	public Function getcommandesbyid($id){
 		$sth = $this->Bdd->prepare("
-									SELECT nom_produit,prix_produit,poids_produit,description_produit,stock_produit FROM T_produits 
-									WHERE id=(:id)");
+									SELECT id_commande,date_commande FROM T_commandes 
+									WHERE id_client=(:id)");
 		$sth->execute(array(':id' => $id));
 			
 		return $sth->fetchAll();
